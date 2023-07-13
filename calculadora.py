7971309f956ca004ab83f0d500168d854f070857
Hernan Fresco <demostraciongit@gmail.com>
2023-07-12 21:54:53 -0300

    desarrollo calculadora

---
+++ calculadora.py
@@ -0,0 +1,6 @@
+import enteros as e
+
+valor1= float(input("Ingrese un numero: "))
+valor2= float(input("Ingrese otro numero: "))
+print(f"La suma entre {valor1} y {valor2} es {e.sumar(valor1,valor2)}")
+print(f"La multiplicacion entre {valor1} y {valor2} es {e.producto(valor1,valor2)}")
